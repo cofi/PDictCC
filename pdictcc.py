d98d8456af6fa811eb10755c8604b70f40cfe1f1
Michael Markert <markert.michael@googlemail.com>
2012-02-24 23:25:47 +0100

    Refine the argument parser.
    Signed-off-by: Michael Markert <markert.michael@googlemail.com>

--- pdictcc.py
+++ pdictcc.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+__version__ = ('0', '1')
+
 if __name__ == '__main__':
     import sys
     import argparse
@@ -12,22 +14,23 @@ if __name__ == '__main__':
                                                  for opts in
                                                  ['[-i DICTCC_FILE]',
                                                   '[-v] [-S] [-h]',
-                                                  '[-s | -r | -f] QUERY']))
+                                                  '[-c] [-s | -r | -f] QUERY']))
 
     db = parser.add_argument_group('Database building options')
-    db.add_argument('-i', '--import', help='Import the dict file from dict.cc')
+    db.add_argument('-i', '--import', metavar='DICTCC_FILE',
+                    help='Import the dict file from dict.cc')
 
     formatting = parser.add_argument_group('Format options')
     formatting.add_argument('-c', '--compact', action='store_true',
                             help='Use compact output format')
 
     misc = parser.add_argument_group('Misc options')
-    misc.add_argument('-v', '--version', action='store_true',
-                      help='Show version')
+    misc.add_argument('-v', '--version', action='version', help='Show version',
+                      version='%(prog)s {0}'.format('.'.join(__version__)))
     misc.add_argument('-S', '--size', action='store_true',
                       help='Show the number of entries in the databases')
-    misc.add_argument('-d', '--directory',
-                      help='Use DIRECTORY instead of ~/.rdictcc')
+    misc.add_argument('-d', '--directory', metavar='PATH',
+                      help='Use PATH instead of ~/.rdictcc')
     misc.add_argument('-h', '--help', action='help',
                       help='Show this help message and exit')
 
@@ -39,4 +42,7 @@ if __name__ == '__main__':
                        help='Translate all the words mathing the regexp QUERY')
     query_mutex.add_argument('-f', '--fulltext', action='store_true',
                        help='Translate all sentences matching the regexp QUERY')
-    args = parser.parse_args(sys.argv)
+    parser.add_argument('query', metavar='QUERY', nargs='*',
+                        help='the query to search')
+
+    args = parser.parse_args()
